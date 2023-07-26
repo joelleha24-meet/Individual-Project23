7ad7522db42948afce97e14d7b8d87fb60e4581b
joelleha24-meet <139680897+joelleha24-meet@users.noreply.github.com>
2023-07-26 14:29:25 +0300

    a

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -2,15 +2,87 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {'apiKey': "AIzaSyAV4nP0Aw0CvO-ZSHq72-4d521IRimIh6M",
+  'authDomain': "joelle-project-268cc.firebaseapp.com",
+  'projectId': "joelle-project-268cc",
+  'storageBucket': "joelle-project-268cc.appspot.com",
+  'messagingSenderId': "782530638490",
+  'appId': "1:782530638490:web:84c3485f302ef175ae6523",
+  "databaseURL": ""}
+
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        age = int(request.form['age'])
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {"age":age, "email": email,"password":password}
+            db.child("User").child(UID).set(user)
+            
+            if age <= 0 or not email or not password:
+                error_msg = "Error, please try again, make sure that your age is bigger than 0, and that you enter your name and your last name, thank you!"
+                return render_template('form.html', error_msg=error_msg)
+            elif age >= 18:
+                return redirect('/adult')
+            else:
+                return redirect('/minor')
+        except:
+            return 'submiting failed'
+    return render_template('form.html')
+@app.route('/minor')
+def minor():
+    return render_template('minor.html')
+@app.route('/information')
+def information():
+    return render_template('information.html')
+@app.route('/adult')
+def adult():
+    return render_template('adult.html')
+
+@app.route('/family')
+def family():
+    return render_template('family.html')
+
+@app.route('/parents')
+def parents():
+    return render_template('parents.html')
+
+@app.route('/school')
+def school():
+    return render_template('school.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+@app.route('/college')
+def college():
+    return render_template('college.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
 
 
-
-
-#Code goes above here
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Individual-Project/templates/script.js
@@ -0,0 +1,99 @@
+// // alert('hi, welcome to our website');
+
+// function readMoreFunction() {
+//   window.location.replace('about.html');
+// }
+
+// function ageFidner() {
+//   console.log('funciton was called');
+//   var name = document.getElementById("fname").value;
+//   var lastName = document.getElementById("lname").value;
+//   // document.getElementById("Hinfo").innerHTML = "Hello" + name + "!" + "Before we start here is some information about peer pressure:";
+//   var x = document.getElementById("age").value;
+//   console.log(x);
+//   if (x <= 0 || name == "" || lastName == "") {
+//     alert("Eror, please try again, make sure that your age is bigger than 0, and that you enter your name and your last name, thank you!")
+//   }
+//   else if (x >= 18) {
+//     // const age = adult;
+//     window.location.replace('adult.html');
+//   }
+//   else if (x < 18) {
+//     // const age = minor;
+//     window.location.replace('minor.html');
+//   }
+
+
+// }
+
+// function goBackToMinorPage() {
+//   window.location.replace('minor.html');
+
+// }
+// function goBackToAdultPage() {
+//   window.location.replace('adult.html');
+
+// }
+
+// function goToFormPage() {
+//   window.location.replace('form.html');
+
+// }
+
+// function school() {
+//   window.location.replace('school.html');
+// }
+
+// function familyFun() {
+//   window.location.replace('family.html');
+
+
+// }
+
+// function ifyouareaparent() {
+//   window.location.replace('parents.html');
+// }
+
+// function information() {
+//   window.location.replace('information.html');
+// }
+
+// function work() {
+//   window.location.replace('work.html');
+// }
+// function college() {
+//   window.location.replace('college.html');
+// }
+// function partner() {
+//   window.location.replace('partner.html');
+// }
+// function parents() {
+//   window.location.replace('parents.html');
+// }
+
+// function home() {
+//   window.location.replace('/');
+// }
+
+
+// // function letsStartbtn() {
+// //   if (age == adult) {
+// //     window.location.replace('adult.html');
+// //   }
+// //   else if (age == minor) {
+// //     window.location.replace('minor.html');
+// //   }
+// // }
+
+
+// //let length = window.history.length;
+
+// // // Grab the wrap element
+// // const backButton = document.getElementById('backBtnAdult')
+// // backButton.appendChild(backContent)
+// // // Add className to the button (optional)
+// // backButton.classList.add('my-back-button')
+// // // Put the button into the DOM
+// // wrap.appendChild(backButton)
+// // // Add the click listener to the button
+// // backButton.addEventListener('click', () => window.history.back());
